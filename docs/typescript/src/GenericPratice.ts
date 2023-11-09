388733bc7a11d2dce74dc7c3c15888084b77ae1b
celso <celso.takashi07@gmail.com>
2023-11-08 21:56:35 -0300

    adding generic pratice

--- docs/typescript/src/GenericPratice.ts
+++ docs/typescript/src/GenericPratice.ts
@@ -1,4 +1,6 @@
 // Generic : Additional parameter to fuctions, interface and classes
+// https://www.youtube.com/watch?v=qEGiDfMfHro ,- This video eplain in a better way
+// Generic are used more in Observable and Promises
 
 function returnKeys<T extends Object>(arg: T){
 
@@ -23,7 +25,7 @@ const John: Person<string> = {
     age: 20
 }
 
-// Observable and Promises
+// Generic are used more in Observable and Promises
 class Observable <T extends Person<string>>{
 
     subscribe(arg: T){
