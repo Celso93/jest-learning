0286e58fadb7d5dedbcb985f4679df77bd253dd2
celso <celso.takashi07@gmail.com>
2023-11-27 21:04:23 -0300

    jest basic and intermediary completed

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -15,6 +15,13 @@ describe('Utils test suite', () => {
             console.log('Teardown')
         })
 
+        it('Should throw error on valid argument', ()=>{
+            const actual = sut.toUpperCase('Class')
+            const expected = 'CLASS'
+
+            expect(actual).toBe(expected)
+        })
+
         it('Should throw error on invalid argument - function', ()=>{
             function expectError() {
                 const actual = sut.toUpperCase('');
@@ -29,32 +36,32 @@ describe('Utils test suite', () => {
             }).toThrowError('Invalid argument!');
         })
 
-        it('Should throw error on invalid argument - try catch block', (done)=>{             
-            try {
-                sut.toUpperCase('');
-                done('GetStringInfo should throw error for invalid arg!')
-            } catch (error) {
-                expect(error).toBeInstanceOf(Error);
-                expect(error).toHaveProperty('message', 'Invalid argument!');
-            }
-        })
+        // it('Should throw error on invalid argument - try catch block', () => {
+        // This is confusing.. this will only work with a workaround             
+        //     try {
+        //         sut.toUpperCase('');
+        //     } catch (error) {
+        //         expect(error).toBeInstanceOf(Error);
+        //         expect(error).toHaveProperty('message', 'Invalid argument!');
+        //     }
+        // })
 
     });
 
 
 
-    it('Should return uppercase of a valid string', () => {
-        // arrange
-        // sut = system under test
-        const sut = toUpperCase;
-        const expected = 'ABC'
+    // it('Should return uppercase of a valid string', () => {
+    //     // arrange
+    //     // sut = system under test
+    //     const sut = toUpperCase;
+    //     const expected = 'ABC'
 
-        // ACT
-        const actual = sut('abc');
+    //     // ACT
+    //     const actual = sut('abc');
         
-        // assert/matchers;
-        expect(actual).toBe(expected)
-    })
+    //     // assert/matchers;
+    //     expect(actual).toBe(expected)
+    // })
 
     // Best pratice to run paramitezed test
     describe('toUpperCase examples', () => {
@@ -94,27 +101,27 @@ describe('Utils test suite', () => {
         });
     })
 
-    it('Should return info for valid string', () => {
-        const actual = getStringInfo('My-String')
+    // it('Should return info for valid string', () => {
+    //     const actual = getStringInfo('My-String')
 
-        expect(actual.lowerCase).toBe('my-string')
-        expect(actual.extraInfo).toEqual({})
+    //     expect(actual.lowerCase).toBe('my-string')
+    //     expect(actual.extraInfo).toEqual({})
 
-        expect(actual.characters.length).toBe(9)
-        expect(actual.characters).toHaveLength(9)
+    //     expect(actual.characters.length).toBe(9)
+    //     expect(actual.characters).toHaveLength(9)
 
-        expect(actual.characters).toContain<string>('M')
-        expect(actual.characters).toEqual(
-            expect.arrayContaining(['M', 'y', '-', 'S', 't', 'r', 'i', 'n', 'g'])    
-        )
+    //     expect(actual.characters).toContain<string>('M')
+    //     expect(actual.characters).toEqual(
+    //         expect.arrayContaining(['M', 'y', '-', 'S', 't', 'r', 'i', 'n', 'g'])    
+    //     )
 
-        expect(actual.extraInfo).not.toBe(undefined)
-        expect(actual.extraInfo).not.toBeUndefined()
-        expect(actual.extraInfo).toBeDefined()
-        expect(actual.extraInfo).toBeTruthy()
+    //     expect(actual.extraInfo).not.toBe(undefined)
+    //     expect(actual.extraInfo).not.toBeUndefined()
+    //     expect(actual.extraInfo).toBeDefined()
+    //     expect(actual.extraInfo).toBeTruthy()
 
-        // primitive type = toBe
-        // complex type
-    })
+    //     // primitive type = toBe
+    //     // complex type
+    // })
 
 })
